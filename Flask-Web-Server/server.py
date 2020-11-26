8a09f544bd311235dbc2350b33675d7df99b6529
Ga-Yeon Park <rkdusdd52@naver.com>
2020-11-26 15:46:30 +0900

    ㅇ

--- Flask-Web-Server/server.py
+++ Flask-Web-Server/server.py
@@ -11,25 +11,27 @@ app = Flask(__name__)
 
 loaded_model = load_model('Flask-Web-Server/model/best_model.h5')
 
+
 def sentiment_predict(new_sentence):
-  new_sentence = TreebankWordTokenizer().tokenize(new_sentence)#토큰화
+  new_sentence = TreebankWordTokenizer().tokenize(new_sentence)  # 토큰화
   #new_sentence = [word for word in new_sentence if not word in stopwords] # 불용어 제거
-  encoded = Tokenizer.texts_to_sequences([new_sentence]) # 정수 인코딩
+  encoded = Tokenizer.texts_to_sequences([new_sentence])  # 정수 인코딩
   print(encoded)
-  pad_new = pad_sequences(encoded, maxlen = 42) # 패딩
+  pad_new = pad_sequences(encoded, maxlen=42)  # 패딩
   #print(pad_new)
-  score = float(loaded_model.predict(pad_new)) # 예측
+  score = float(loaded_model.predict(pad_new))  # 예측
   return score
 
 
-@app.route("/", methods=['GET','POST'])
+@app.route("/", methods=['GET', 'POST'])
 def index():
-	if request.method == 'GET':
-		return render_template('index.html')
-	if request.method == 'POST':
-		sentence = float(request.form['inputext'])
-		score = sentiment_predict(sentence)
-		return render_template('index.html', result = score)
+  sentence = ""
+  if request.method == 'GET':
+    return render_template('index.html')
+  if request.method == 'POST':
+    sentence = request.form.get['inputext']
+    score = float(sentiment_predict(sentence))
+    return render_template('index.html', result = score)
 
 if __name__ == '__main__':
 	app.run(debug=True)
